e3fce0cdde6615d5720d8a0f9db983757a78b973
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-03-14 05:08:34 +0100

    Inicialitzed World, and testing World/Rooms class

--- My Zork! - The Lost Sock/main.cpp
+++ My Zork! - The Lost Sock/main.cpp
@@ -1,1 +1,17 @@
-#include "zork.h"
+#include <stdio.h>
+#include <string.h>
+
+#include "world.h"
+
+int main()
+{
+	World Sock;
+
+	strcpy_s(Sock.room[0].name, "hi");
+
+	printf("%s", Sock.room[0].name);
+
+	getchar();
+
+	return 0;
+}
