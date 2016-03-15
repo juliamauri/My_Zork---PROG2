165890131b6faf7dffd328ee0a170b9e196930ab
Julià Mauri Costa <juliamauricosta@gmail.com>
2016-03-15 09:50:11 +0100

    Initied variables of rooms

--- My Zork! - The Lost Sock/world.cpp
+++ My Zork! - The Lost Sock/world.cpp
@@ -30,7 +30,8 @@ void World::CreateWorld()
 	player[0].pos = 1;
 
 	strcpy_s(room[0].name, "Bedroom"); 
-
+	strcpy_s(room[0].desc, "Desc 1");
+	room[0].world = 1;
 	room[0].n_room = 1;
 	room[0].n = 0;
 	room[0].s= 0;
@@ -38,7 +39,8 @@ void World::CreateWorld()
 	room[0].w = 0;
 
 	strcpy_s(room[1].name, "Living room");
-	
+	strcpy_s(room[1].desc, "Desc 2");
+	room[1].world = 1;
 	room[1].n_room = 2;
 	room[1].n = 3;
 	room[1].s = 0;
@@ -46,7 +48,8 @@ void World::CreateWorld()
 	room[1].w = 1;
 	
 	strcpy_s(room[2].name, "Entrance hall"); 
-	
+	strcpy_s(room[1].desc, "Desc 2");
+	room[1].world = 1;
 	room[2].n_room = 3;
 	room[2].n = 0;
 	room[2].s = 2;
@@ -54,7 +57,8 @@ void World::CreateWorld()
 	room[2].w = 0;
 	
 	strcpy_s(room[3].name, "Kitchen");
-
+	strcpy_s(room[3].desc, "Desc 4");
+	room[3].world = 1;
 	room[3].n_room = 4;
 	room[3].n = 0;
 	room[3].s = 5;
@@ -62,7 +66,8 @@ void World::CreateWorld()
 	room[3].w = 2;
 
 	strcpy_s(room[4].name, "Storeroom");
-
+	strcpy_s(room[4].desc, "Desc 5");
+	room[4].world = 1;
 	room[4].n_room = 5;
 	room[4].n = 4;
 	room[4].s = 0;
@@ -70,11 +75,59 @@ void World::CreateWorld()
 	room[4].w = 0;
 
 	strcpy_s(room[5].name, "Machines room");
+	strcpy_s(room[5].desc, "Desc 6");
+	room[5].world = 2;
+	room[5].n_room = 6;
+	room[5].n = 0;
+	room[5].s = 7;
+	room[5].e = 0;
+	room[5].w = 0;
+
 	strcpy_s(room[6].name, "Drying room");
+	strcpy_s(room[6].desc, "Desc 7");
+	room[6].world = 2;
+	room[6].n_room = 7;
+	room[6].n = 6;
+	room[6].s = 0;
+	room[6].e = 8;
+	room[6].w = 0;
+
 	strcpy_s(room[7].name, "Research room");
+	strcpy_s(room[7].desc, "Desc 8");
+	room[7].world = 2;
+	room[7].n_room = 8;
+	room[7].n = 4;
+	room[7].s = 9;
+	room[7].e = 10;
+	room[7].w = 7;
+
 	strcpy_s(room[8].name, "Technology room");
+	strcpy_s(room[8].desc, "Desc 9");
+	room[8].world = 2;
+	room[8].n_room = 9;
+	room[8].n = 8;
+	room[8].s = 0;
+	room[8].e = 0;
+	room[8].w = 0;
+
 	strcpy_s(room[9].name, "Boss room");
+	strcpy_s(room[9].desc, "Desc 10");
+	room[9].world = 2;
+	room[9].n_room = 10;
+	room[9].n = 11;
+	room[9].s = 0;
+	room[9].e = 0;
+	room[9].w = 8;
+
 	strcpy_s(room[10].name, "Store socks");
+	strcpy_s(room[10].desc, "Desc 11");
+	room[10].world = 2;
+	room[10].n_room = 11;
+	room[10].n = 0;
+	room[10].s = 10;
+	room[10].e = 0;
+	room[10].w = 0;
+
 
 	for (int i = 0; i < 11; i++)
 	{
